4659a5e897e577c6804695a06c7dab54775a56c4
Maggie Greene <maggie.greene96@gmail.com>
2019-10-30 20:20:03 -0500

    Added login and logout

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,17 @@
+import firebase from 'firebase';
+import apiKeys from './helpers/apiKeys.json';
 import 'bootstrap';
 import '../styles/main.scss';
+import login from './components/Auth/auth';
+import authData from './helpers/data/authData';
+import myNavbar from './components/MyNavbar/myNavbar';
+
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
+  login.loginButton();
+  login.signMeIn();
+  myNavbar.logoutEvent();
+};
+
+init();
