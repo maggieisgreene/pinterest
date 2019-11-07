10adcb1b2b4bc0b2d715377971e2312c2ccde62b
Maggie Greene <maggie.greene96@gmail.com>
2019-11-06 18:33:55 -0600

    Boards print by UID

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,14 +5,12 @@ import '../styles/main.scss';
 import login from './components/Auth/auth';
 import authData from './helpers/data/authData';
 import myNavbar from './components/MyNavbar/myNavbar';
-import boards from './components/AllBoards/allBoards';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseConfig);
   authData.checkLoginStatus();
   login.loginButton();
   myNavbar.logoutEvent();
-  boards.printBoards();
 };
 
 init();
